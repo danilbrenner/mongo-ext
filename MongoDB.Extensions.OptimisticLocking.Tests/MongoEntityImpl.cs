7fb503dde8c7bde2e049e091fd387da5e0cd19fa
Danil <danilbrenner@outlook.com>
2020-01-02 11:56:07 +0200

    Init

---
+++ MongoDB.Extensions.OptimisticLocking.Tests/MongoEntityImpl.cs
@@ -0,0 +1,8 @@
+namespace MongoDB.Extensions.OptimisticLocking.Tests
+{
+    public class MongoEntityImpl
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
